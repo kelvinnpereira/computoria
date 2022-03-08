7c59c68748271ea2f8848b169bb2bfec4173a8e2
kelvinnpereira <kdsnp@icomp.ufam.edu.br>
2022-03-07 22:40:16 -0400

    setando corretamente o process.env.NEXT_PUBLIC_URL

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const HOST = 'http://' + (process.env?.NEXT_PUBLIC_URL ? process.env.NEXT_PUBLIC_URL : "localhost:3000");
+const HOST = 'http://' + (process.env.NEXT_PUBLIC_URL ? process.env.NEXT_PUBLIC_URL : "localhost:3000");
 
 const server = axios.create({
   baseURL: HOST,
