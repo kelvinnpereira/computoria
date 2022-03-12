5190add097b3f74aa320dff87e767ff52cb3b3f7
kelvinnpereira <kdsnp@icomp.ufam.edu.br>
2022-03-12 15:28:13 -0400

    ajustando o nome do diretorio

--- database/generate.js
+++ database/generate.js
@@ -5,7 +5,7 @@ fs.readdirSync(__dirname + '/../app/models').filter(file => {
     return (file.indexOf('.') !== 0) && (!file.startsWith('index')) && (file.slice(-3) === '.js');
 }).forEach(file => {
     let base = file.slice(0, -3);
-    let req = '\t' + base + ': require(\"'+ __dirname.replaceAll('\\', '/') + '/../app/models/' + base + '\"), \n';
+    let req = '\t' + base + ': require(__dirname + \"/../app/models/' + base + '\"), \n';
     fs.appendFileSync(__dirname + '/models.js', req);
 });
 fs.appendFileSync(__dirname + '/models.js', '}');
